41e51c22bf0d0a45ae85c60fcab088bef32156a6
kingrayhan <rayhan095@gmail.com>
2021-10-10 23:29:08 +0600

    ep5

---
+++ api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create(
+            array_merge(
+                $request->only('username', 'email', 'password'),
+                ['password' => bcrypt($request->password)]
+            )
+        );
+
+        return [
+            'message' => 'User registered successfully',
+            'data' => $user
+        ];
+    }
+
+    public function login()
+    {
+
+    }
+
+
+    public function logout()
+    {
+
+    }
+}
