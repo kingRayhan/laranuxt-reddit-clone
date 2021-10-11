d5d019a9bf5f265625d9ac2696503ffc00821f55
kingrayhan <rayhan095@gmail.com>
2021-10-12 01:16:29 +0600

    ep9

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -2,15 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+
+    /**
+     * Register a new user
+     * @param RegisterRequest $request
+     * @return array
+     */
     public function register(RegisterRequest $request)
     {
-        $user = User::create( $request->only('username', 'email', 'password'));
+        $user = User::create($request->only('username', 'email', 'password'));
 
         return [
             'message' => 'User registered successfully',
@@ -18,9 +27,21 @@ class AuthController extends Controller
         ];
     }
 
-    public function login()
-    {
 
+    /**
+     * Login user
+     * @param LoginRequest $request
+     */
+    public function login(LoginRequest $request)
+    {
+        if (!auth()->attempt($request->only('email', 'password'))) {
+            throw new AuthenticationException('Invalid credential');
+        }
+
+        return [
+            'message' => 'login successful',
+            'token' => auth()->user()->createToken('access-token')->plainTextToken
+        ];
     }
 
 
